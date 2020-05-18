ae94f83110c5809d50cb7689cc3ac3ae92cc8f8f
Emmanuel-Garnica <56373024+Emmanuel-Garnica@users.noreply.github.com>
2020-05-18 18:09:12 -0500

    ENDPOINT REGISTRO - Se añadió el endpoint de registro de usuarios, agregando la librería 'bcrypt' para hashear.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 const sequelize = new Sequelize('mysql://root:@localhost:3306/delilah_resto');
@@ -57,6 +59,25 @@ app.delete('/productos', (req, res) => {
 
 });
 
+app.post('/usuarios', (req, res) => {
+
+    const { nombre_y_apellido, email, telefono, direccion, contrasena } = req.body;
+
+    const hash = bcrypt.hashSync(contrasena, 10);
+    console.log(hash);
+    sequelize.query(
+        'INSERT INTO usuarios ( nombre_y_apellido, email, telefono, direccion, hash) VALUES (?,?,?,?,?)', { replacements: [nombre_y_apellido, email, telefono, direccion, hash] }
+    ).then(resultados => {
+        console.log(resultados)
+        res.status(201).json({ id: resultados[0], nombre_y_apellido, email, telefono, direccion });
+    });
+
+});
+
 app.listen(3000, () => {
+    // const pass = "HolaSoyYo1960";
+    // const hash = await bcrypt.hash(pass, 10);
+    // console.log(hash);
+    // console.log(typeof(hash))
     console.log('Servidor iniciado en el puerto 3000. ')
 });
